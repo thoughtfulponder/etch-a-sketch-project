234020f4b96776a4edd73ed7d69f33c53a8a4e4d
thoughtfulponder <61815746+thoughtfulponder@users.noreply.github.com>
2025-11-11 23:05:49 +0530

    trying to create square divs this commit and push is to just transfer codes into a different device

--- myscript.js
+++ myscript.js
@@ -1,1 +1,34 @@
 const container = document.querySelector("#container");
+
+function makeDivs(nums) {
+  for (var i = 0; i < nums; i++) {
+    let row = document.createElement("div");
+    row.classList.add("item");
+    container.appendChild(row);
+  }
+}
+
+makeDivs(4);
+
+/*function makeRows(nums) {
+  for (let i = 0; i < nums; i++) {
+    let gridRows = document.createElement("div");
+    gridRows.classList.add("grid-rows");
+    container.appendChild(gridRows);
+  }
+}*/
+
+/*function makeGrids(nums) {
+  for (var i = 0; i < nums; i++) {
+    let gridCells = document.createElement("div");
+    gridCells.classList.add("grid-cells");
+    for (var j = 0; j < nums; j++) {
+      let gridRows = document.createElement("div");
+      gridRows.classList.add("grid-rows");
+      gridCells.appendChild(gridRows);
+    }
+    container.appendChild(gridCells)
+  }
+}
+
+makeGrids(16);*/
