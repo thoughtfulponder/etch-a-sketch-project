0c94b32ebb2a227ba3f82987e61a994a52562653
thoughtfulponder <61815746+thoughtfulponder@users.noreply.github.com>
2025-11-26 17:22:47 +0000

    added event listener to gridCells

--- myscript.js
+++ myscript.js
@@ -9,7 +9,7 @@ function makeGrid(nums) {
   let value = nums * nums;
   for (let i = 0; i < value; i++) {
     let gridCells = document.createElement("div");
-    gridCells.className = "grid-cells";
+    gridCells.classList.add("grid-cells");
     container.appendChild(gridCells);
     gridCells.style.setProperty("flex-basis", `calc(100% / ${nums})`);
   }
@@ -17,21 +17,21 @@ function makeGrid(nums) {
 }
 
 btn.addEventListener("click", () => {
-   /*for (let i = 0; i < gridCells.length; i++) {
-    container.removeChild(gridCells[i]);
-  }*/
+  /*for (let i = 0; i < gridCells.length; i++) {
+   container.removeChild(gridCells[i]);
+ }*/
   container.innerHTML = "";
-  
+
   num = prompt("enter a number");
   if (num > 100 || num === null || num === "") {
     alert("please enter a between 1 to 100");
     gridValue = 16;
     makeGrid(gridValue);
-    }
-    else {
-      gridValue = num;
-      makeGrid(gridValue);
-    }
+  }
+  else {
+    gridValue = num;
+    makeGrid(gridValue);
+  }
 });
 
 
@@ -41,12 +41,8 @@ console.log(grid);
 let gridCells = document.querySelectorAll(".grid-cells");
 
 
-
-
-
-
-
-
-
-
-
+gridCells.forEach(cell => {
+  cell.addEventListener("click", () => {
+    cell.classList.toggle("pressed")
+  });
+});
