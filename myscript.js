0206a95c77b926e84694f08c6d560bdc7b222e5f
thoughtfulponder <61815746+thoughtfulponder@users.noreply.github.com>
2025-11-27 17:25:32 +0530

    successful wrote the logic to remove square divs using for loop and gridCells length and also successfully able to change gridCells length as needed

--- myscript.js
+++ myscript.js
@@ -2,10 +2,12 @@ const container = document.querySelector("#container");
 
 const btn = document.querySelector(".btn");
 
+
+
 let gridValue = 16;
 
 function makeGrid(nums) {
-
+  
   let value = nums * nums;
   for (let i = 0; i < value; i++) {
     let gridCells = document.createElement("div");
@@ -16,13 +18,21 @@ function makeGrid(nums) {
   return value
 }
 
-btn.addEventListener("click", () => {
-  /*for (let i = 0; i < gridCells.length; i++) {
-   container.removeChild(gridCells[i]);
- }*/
-  container.innerHTML = "";
+let grid = makeGrid(gridValue);
+console.log(grid);
 
-  num = prompt("enter a number");
+/*let gridCells = Array.from(container.childNodes);*/
+
+let gridCells = document.querySelectorAll(".grid-cells");
+
+btn.addEventListener("click", () => {
+  for (let i = 0; i < gridCells.length; i++) {
+   /*container.removeChild(gridCells[i]);*/
+   gridCells[i].remove();
+ }
+  /*container.innerHTML = "";*/
+  setTimeout( () => {
+    num = prompt("enter a number");
   if (num > 100 || num === null || num === "") {
     alert("please enter a between 1 to 100");
     gridValue = 16;
@@ -32,17 +42,30 @@ btn.addEventListener("click", () => {
     gridValue = num;
     makeGrid(gridValue);
   }
+  /*gridCells = Array.from(container.childNodes);*/
+  gridCells = document.querySelectorAll(".grid-cells");
+  }, 10);
+  
 });
 
+gridCells = document.querySelectorAll(".grid-cells");
 
-let grid = makeGrid(gridValue);
-console.log(grid);
 
-let gridCells = document.querySelectorAll(".grid-cells");
+
+
+/*let gridCells = document.querySelectorAll(".grid-cells");*/
+
+/*let gridCells = Array.from(container.childNodes);*/
 
 
 gridCells.forEach(cell => {
   cell.addEventListener("click", () => {
-    cell.classList.toggle("pressed")
+    cell.classList.add("pressed")
   });
 });
+
+/*for (let i = 0; i < gridCells.length; i++) {
+  gridCells[i].addEventListener("click", () => {
+    gridCells[i].classList.add("pressed");
+  });
+}*/
